0639b52364d9d684cc3c77f8dbfcc989bb2a953a
Ahmad <snankls@gmail.com>
2025-05-06 20:52:09 +0500

    fixed route

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect } from 'react';
+//import React, { useContext, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator';
+//import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <AppNavigator />
+        {/* <AppNavigator /> */}
       </NavigationContainer>
     </AuthProvider>
   );
